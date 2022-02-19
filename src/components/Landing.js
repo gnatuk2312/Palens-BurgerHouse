a601a01f6804eb2017a8d0df72a9af8ebf84039f
gnatuk2312 <84677177+gnatuk2312@users.noreply.github.com>
2022-02-19 14:11:16 +0200

    component Landing.js
    Повноцінна робота елемента "виберіть ресторан", Окрім кнопки переходу на сайт кожного з ресторанів

---
+++ src/components/Landing.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import restaurants from '../sample-restaurants';
+
+class Landing extends React.Component {
+	state = {
+		display: false,
+		title: '',
+		url: ''
+	};
+
+
+	displayList = () => {
+		// const display = this.state.display;
+		const { display } = this.state;
+		this.setState({ display: !display });
+	}
+
+	getTitle = (restaurant) => {
+		// const title = restaurant.title
+		// const url = restaurant.url
+		const { title, url } = restaurant;
+		this.setState({
+			// title: title,
+			title,
+			// url: url,
+			url,
+			display: false
+		})
+	}
+
+	goToRestaurant = () => {
+		console.log('gooooo mFuckeeerr!');
+
+	}
+
+	render() {
+
+		return (
+			<div className='restaurant_select'>
+				<div className='restaurant_select_top'>
+					<div onClick={this.displayList} className='restaurant_select_top-header font-effect-outline'>
+						{this.state.title ? this.state.title : 'Вибери ресторан'}
+					</div>
+					<div className='arrow_picker'>
+						<div className='arrow_picker-up'></div>
+						<div className='arrow_picker-down'></div>
+					</div>
+				</div>
+				{this.state.display ? <div className='restaurant_select_bottom'>
+					<ul>
+						{restaurants.map(restaurant => {
+							return <li onClick={() => this.getTitle(restaurant)} key={restaurant.id}>{restaurant.title}</li>
+						})}
+					</ul>
+				</div> : null}
+
+				{this.state.title && !this.state.display ? (
+					<button onClick={this.goToRestaurant}>Перейти в ресторан</button>
+				) : null}
+			</div>
+
+		)
+	}
+}
+
+export default Landing;
