7f156db4a8a9ad32b05a50193a15955aee9871c2
gnatuk2312 <84677177+gnatuk2312@users.noreply.github.com>
2022-02-22 14:22:47 +0200

    створив компонент AddBurgerForm.js
    Добавив цей компонент в MenuAdmin і зробив так що всі дані з інпутів AddBurgerForm записуються в об'єкт state компонента App.js

---
+++ src/components/AddBurgerForm.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class AddBurgerForm extends React.Component {
+	nameRef = React.createRef();
+	priceRef = React.createRef();
+	statusRef = React.createRef();
+	descRef = React.createRef();
+	imageRef = React.createRef();
+
+	createBurger = (event) => {
+		event.preventDefault();
+		const burger = {
+			name: this.nameRef.current.value,
+			price: this.priceRef.current.value,
+			status: this.statusRef.current.value,
+			desc: this.descRef.current.value,
+			image: this.imageRef.current.value
+		}
+
+		this.props.addBurger(burger);
+
+		event.currentTarget.reset();
+
+	}
+
+	render() {
+		return (
+			<form className='burger-edit' onSubmit={this.createBurger}>
+				<input ref={this.nameRef} name='name' type='text' placeholder='Назва' autoComplete='off'></input>
+				<input ref={this.priceRef} name='price' type='text' placeholder='Ціна' autoComplete='off'></input>
+				<select ref={this.statusRef} name='status' className='status'>
+					<option value='available'>Доступно</option>
+					<option value='unavailable'>Забрати з меню</option>
+				</select>
+				<textarea ref={this.descRef} name='desc' placeholder='Опис'></textarea>
+				<input ref={this.imageRef} name='image' type='text' placeholder='Зображення' autoComplete='off'></input>
+				<button type='submit'>Добавити в меню</button>
+			</form>
+		)
+	}
+}
+export default AddBurgerForm; 
--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,21 @@ import MenuAdmin from './MenuAdmin';
 
 
 class App extends React.Component {
+
+	state = {
+		burgers: {},
+		order: {}
+	}
+
+	addBurger = (burger) => {
+		// 1. Робимо копію об'єкта state
+		const burgers = { ...this.state.burgers };
+		// 2. Добавити новий бургер в об'єкт burgers
+		burgers[`burger${Date.now()}`] = burger;
+		// 3. Записати наш новий об'єкт burgers в state
+		this.setState({ burgers: burgers });
+	}
+
 	render() {
 		return (
 			<div className="burger-paradise">
@@ -12,7 +27,7 @@ class App extends React.Component {
 					<Header title="Palens BurgerHouse" />
 				</div>
 				<Order />
-				<MenuAdmin />
+				<MenuAdmin addBurger={this.addBurger} />
 			</div>
 		)
 	}
