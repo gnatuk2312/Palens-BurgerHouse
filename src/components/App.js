1e9b6ce0013b7e491a8345870beea7d607fe72e5
gnatuk2312 <84677177+gnatuk2312@users.noreply.github.com>
2022-02-22 15:39:17 +0200

    прописаний компонент Burger.js
    Рендеряться всі бургери які є в state.
    Коли нажимаєш кнопку замовити в state.order записуються дані про замовлення

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import MenuAdmin from './MenuAdmin';
+import Burger from './Burger';
 import sampleBurgers from '../sample-burgers';
 
 
@@ -23,14 +24,32 @@ class App extends React.Component {
 
 	loadSampleBurgers = () => {
 		this.setState({ burgers: sampleBurgers })
-
 	};
+
+	AddToOrder = (key) => {
+		//1. Робимо копію об'єкта state
+		const order = { ...this.state.order };
+		//2. Добавити ключ до замовлення зі значенням 1, або обновити значення
+		order[key] = order[key] + 1 || 1;
+		// 3. Записати наш новий об'єкт burgers в state
+		this.setState({ order: order });
+	}
 
 	render() {
 		return (
 			<div className="burger-paradise">
 				<div className="menu">
 					<Header title="Palens BurgerHouse" />
+					<ul className='burgers'>
+						{Object.keys(this.state.burgers).map(key => {
+							return <Burger
+								key={key}
+								index={key}
+								AddToOrder={this.AddToOrder}
+								details={this.state.burgers[key]}
+							/>
+						})}
+					</ul>
 				</div>
 				<Order />
 				<MenuAdmin
